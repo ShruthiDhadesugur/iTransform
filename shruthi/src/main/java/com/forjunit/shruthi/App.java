1d398ccc97fb4022b4d687d79019107af66ed607
ShruthiDhadesugur <shruhi.dhadesugur@gmail.com>
2019-10-17 17:03:23 +0530

    New changes

---
+++ shruthi/src/main/java/com/forjunit/shruthi/App.java
@@ -0,0 +1,14 @@
+package com.forjunit.shruthi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
